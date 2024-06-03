1e0c0d153c5ee9e246d6ffed478b2ef6fdd23ff7
Stiven Carvajal <jostcarvajal223@umariana.edu.co>
2024-06-02 20:02:45 -0500

    enviando index Samsung y celu

--- carrito.js
+++
@@ -1,7 +0,0 @@
-const btnCart = document.querySelector('.container-icon')
-const containerCartProducts = document.querySelector('.container-cart-productos')
-
-btnCart.addEventListener('click', () => {
-    containerCartProducts.classList.toggle
-    ('hidden-cart')
-})
